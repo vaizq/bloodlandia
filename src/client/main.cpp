142cb086ae16f90afbfb4271a84b6a5de1037a64
Väinö Heijari <vai.hei@hotmail.com>
2025-01-07 14:29:10 +0200

    automatic address

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -3,13 +3,16 @@
 
 
 int main(int argc, char** argv) {
+    const char* addr;
     if (argc != 2) {
         printf("usage: app server-address\n");
-        return -1;
+        addr = "109.204.231.229";
+    } else {
+        addr = argv[1];
     }
 
     try {
-        Game g(argv[1]);
+        Game g(addr);
         g.run();
     } catch(const std::exception& e) {
         fprintf(stderr, "Game failed: %s\n", e.what());
